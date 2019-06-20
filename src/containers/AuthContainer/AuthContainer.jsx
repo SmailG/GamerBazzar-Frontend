0544b8945968f214cd74a50d38178c2e18ea12a0
Smail <smailgalijasevic@gmail.com>
2019-06-20 15:44:42 +0200

    registration screen

--- src/containers/AuthContainer/AuthContainer.jsx
+++ src/containers/AuthContainer/AuthContainer.jsx
@@ -15,43 +15,48 @@ class AuthContainer extends Component {
         this.props.history.push('/register');
     }
 
+    alreadyRegistered = (e) => {
+        e.preventDefault();
+        this.props.history.push('/login');
+    }
+
     loginContent = (
         <div className="login-modal">
             <div className="login-modal_header">
                 <h2>Login</h2>
             </div>
 
-            <div className="login-modal_social">
-                <a className="login-modal_social-btn">
+            <div className="auth-modal_social">
+                <a className="auth-modal_social-btn">
                     <SvgLoader name="facebook-social"/>
                 </a>
 
-                <a className="login-modal_social-btn">
+                <a className="auth-modal_social-btn">
                     <SvgLoader name="google-social"/>
                 </a>
 
-                <a className="login-modal_social-btn">
+                <a className="auth-modal_social-btn">
                     <SvgLoader name="twitter-social" />
                 </a>
             </div>
 
-            <div className="login-modal_form">
-                <div className="login-modal_input">
+            <div className="auth-modal_form">
+                <div className="auth-modal_input">
                     <Input placeholder='E-mail Address' />
                 </div>
 
-                <div className="login-modal_input">
+                <div className="auth-modal_input">
                     <Input placeholder='Password' />
                 </div>
 
-                <div className="login-modal_options">
-                    <Checkbox className="login-modal_keep-logged" label={'Keep me logged in'} />
-                    <span className="login-modal_reset-password">
+                <div className="auth-modal_options">
+                    <Checkbox className="auth-modal_keep-logged" label={'Keep me logged in'} />
+                    <span className="auth-modal_reset-password">
                         <a href="">{'Forgot your password?'}</a>
                     </span>
                 </div>
 
-                <div className="login-modal_submit">
+                <div className="auth-modal_submit">
                     <Button>
                         LOGIN
                     </Button>
@@ -65,9 +70,60 @@ class AuthContainer extends Component {
     );
 
     registrationContent = (
-       <div className="registration-modal">
+        <div className="registration-modal">
+            <div className="login-modal">
+                <div className="auth-modal_header">
+                    <h2>Login</h2>
+                </div>
 
-       </div>
+                <div className="auth-modal_social">
+                    <a className="auth-modal_social-btn">
+                        <SvgLoader name="facebook-social" />
+                    </a>
+
+                    <a className="auth-modal_social-btn">
+                        <SvgLoader name="google-social" />
+                    </a>
+
+                    <a className="auth-modal_social-btn">
+                        <SvgLoader name="twitter-social" />
+                    </a>
+                </div>
+
+                <div className="auth-modal_form">
+                    <div className="auth-modal_input">
+                        <Input placeholder='E-mail Address' />
+                    </div>
+
+                    <div className="auth-modal_input">
+                        <Input placeholder='Password' />
+                    </div>
+
+                    <div className="auth-modal_input">
+                        <Input placeholder='Confirm password' />
+                    </div>
+
+                    <div className="auth-modal_submit">
+                        <Button>
+                            LOGIN
+                    </Button>
+                        <span>
+                            {'By signing up you agree to our '}
+                            <a href="">{' Terms of service '}</a>
+                            {'and our'}
+                            <a href="">{' Privacy policy'}</a>
+                        </span>
+                    </div>
+
+                    <div className="auth-modal_options">
+                        <span className="auth-modal_registered">
+                            {'Already have an account ?'}
+                            <a onClick={this.alreadyRegistered} href="">{' Sign in'}</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
     
     render() {
